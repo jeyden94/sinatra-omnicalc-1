009973b25fd52a4c0becd813295ad950b3936cbb
jeyden94 <jeyden94@gmail.com>
2024-10-22 18:44:46 +0000

    Completed square root

--- app.rb
+++ app.rb
@@ -1,6 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/square_root/new") do
+  erb(:hello)
+end
+
+get("/square/results") do
+  erb(:bye)
+end
+
+
+
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+
+  erb(:square_results)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
