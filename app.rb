dae13f35f5cd29a505b9bb949a2af659a7bbbdbb
jeyden94 <jeyden94@gmail.com>
2024-10-22 19:54:11 +0000

    More updates, finished with square, square roots, and payment

--- app.rb
+++ app.rb
@@ -2,12 +2,51 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = Math.sqrt(@the_num)
+
+  erb(:square_root_results)
+end
+
+
+
+get("/payment/new") do
+  erb(:new_payment_calc)
+end
+
+get("/payment/results") do
+  
+  @the_APR = params.fetch("users_apr").to_f
+  @the_rate = params.fetch("users_apr").to_f / 100 / 12
+  @the_num_of_years = params.fetch("users_num_of_years").to_i  
+  @the_n = params.fetch("users_num_of_years").to_i * 12 
+  @the_principal = params.fetch("users_principal").to_f
+
+  @the_numerator = (@the_rate) * (@the_principal)
+  @the_denominator = 1 - ((1 + (@the_rate)) ** (@the_n * -1))
+
+  @the_result = (@the_numerator / @the_denominator).round(2)
+
+
+  erb(:payment_results)
+end
+
+
+
+get("/random/new") do
   erb(:hello)
 end
 
-get("/square/results") do
+get("/random/results") do
   erb(:bye)
 end
+
+
 
 
 
